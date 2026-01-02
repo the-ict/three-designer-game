76b82bc17bb0afc02e5d96fa5404643506bc4b96
the-ict <dvltinv@gmail.com>
2026-01-02 18:08:42 +0500

    feat: Render Saxna 3D model by integrating react-three-fiber and drei.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Model } from "./components/Saxna";
+
 export default function App() {
   return (
-    <div>App</div>
+    <Canvas camera={{ position: [10, 10, 10] }} className="w-full h-screen bg-black text-white">
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[10, 10, 10]} intensity={1} />
+      <Suspense fallback={<div> Loading... </div>}>
+        <Model />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
   )
 }
