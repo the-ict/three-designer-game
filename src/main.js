384730aff7ca8f1ee0cb61767792bfdec029e60d
the-ict <dvltinv@gmail.com>
2026-01-03 20:55:55 +0500

    style: reorder THREE import statement

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
